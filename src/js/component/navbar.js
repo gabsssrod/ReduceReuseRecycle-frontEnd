a3f7aef65b00642650ca8c9bc299a4936f72c1ab
JordanAguiriano <JordanAguiriano@hotmail.com>
2020-01-20 01:28:29 +0000

    fixed nav

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,11 +6,9 @@ import "../../styles/navbar.scss";
 export const Navbar = () => {
 	return (
 		<header>
-			<div className="jumbotron jumbotron-fluid">
+			<div className="header header1">links</div>
+			<div className="jumbotron1 jumbotron-fluid">
 				<div className="container">
-					<button type="button" className="btn btn-primary">
-						Log In
-					</button>
 					<h1 className="display-4">Fluid jumbotron</h1>
 					<p className="lead">
 						This is a modified jumbotron that occupies the entire horizontal space of its parent.
