599af75d40afe574139987eff366210b523f7bb7
JordanAguiriano <JordanAguiriano@hotmail.com>
2020-01-20 00:55:23 +0000

    button

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -8,6 +8,9 @@ export const Navbar = () => {
 		<header>
 			<div className="jumbotron jumbotron-fluid">
 				<div className="container">
+					<button type="button" className="btn btn-primary">
+						Log In
+					</button>
 					<h1 className="display-4">Fluid jumbotron</h1>
 					<p className="lead">
 						This is a modified jumbotron that occupies the entire horizontal space of its parent.
