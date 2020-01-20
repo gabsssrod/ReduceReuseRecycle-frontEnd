6f860a0e8a70eb9bd3a651ec896e20d099441dbd
JordanAguiriano <JordanAguiriano@hotmail.com>
2020-01-20 15:02:39 +0000

    jumbotrons

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -7,17 +7,9 @@ export const Navbar = () => {
 	return (
 		<header>
 			<div className="header header1">
-				<button type="button" className="log-inbtn btn-light">
+				<button type="button" className="log-in btn btn-light">
 					Log In
 				</button>
-			</div>
-			<div className="jumbotron1 jumbotron-fluid">
-				<div className="container">
-					<h1 className="display-4">Fluid jumbotron</h1>
-					<p className="lead">
-						This is a modified jumbotron that occupies the entire horizontal space of its parent.
-					</p>
-				</div>
 			</div>
 		</header>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,153 +1,59 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import "../../styles/navbar.scss";
 
-export const Home = () => (
-	<div>
-		<main role="main">
-			{/* Marketing messaging and featurettes
-      ================================================== */}
-			{/* Wrap the rest of the page in another container to center all the content. */}
-			<div className="container marketing">
-				{/* Three columns of text below the carousel */}
-				<div className="row">
-					<div className="col-lg-4">
-						<img
-							className="rounded-circle"
-							src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
-							alt="Generic placeholder image"
-							width={140}
-							height={140}
-						/>
-						<h2>Heading</h2>
-						<p>
-							Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh
-							ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-							Praesent commodo cursus magna.
-						</p>
-						<p>
-							<a className="btn btn-secondary" href="#" role="button">
-								View details »
-							</a>
-						</p>
-					</div>
-					{/* /.col-lg-4 */}
-					<div className="col-lg-4">
-						<img
-							className="rounded-circle"
-							src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
-							alt="Generic placeholder image"
-							width={140}
-							height={140}
-						/>
-						<h2>Heading</h2>
-						<p>
-							Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
-							elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus
-							commodo, tortor mauris condimentum nibh.
-						</p>
-						<p>
-							<a className="btn btn-secondary" href="#" role="button">
-								View details »
-							</a>
-						</p>
-					</div>
-					{/* /.col-lg-4 */}
-					<div className="col-lg-4">
-						<img
-							className="rounded-circle"
-							src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
-							alt="Generic placeholder image"
-							width={140}
-							height={140}
-						/>
-						<h2>Heading</h2>
-						<p>
-							Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum
-							id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-							condimentum nibh, ut fermentum massa justo sit amet risus.
-						</p>
-						<p>
-							<a className="btn btn-secondary" href="#" role="button">
-								View details »
-							</a>
-						</p>
-					</div>
-					{/* /.col-lg-4 */}
+export const Home = () => {
+	return (
+		<div>
+			<div className="jumbotron1 jumbotron-fluid">
+				<div className="container">
+					<h1 className="display-4">Fluid jumbotron</h1>
+					<p className="lead">
+						This is a modified jumbotron that occupies the entire horizontal space of its parent.
+					</p>
 				</div>
-				{/* /.row */}
-				{/* START THE FEATURETTES */}
-				<hr className="featurette-divider" />
-				<div className="row featurette">
-					<div className="col-md-7">
-						<h2 className="featurette-heading">
-							<span className="text-muted">
-								It
-								{"'"}
-								ll blow your mind.
-							</span>
-						</h2>
-						<p className="lead">
-							Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
-							semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus,
-							tellus ac cursus commodo.
-						</p>
-					</div>
-					<div className="col-md-5">
-						<img
-							className="featurette-image img-fluid mx-auto"
-							data-src="holder.js/500x500/auto"
-							alt="Generic placeholder image"
-						/>
-					</div>
-				</div>
-				<hr className="featurette-divider" />
-				<div className="row featurette">
-					<div className="col-md-7 order-md-2">
-						<h2 className="featurette-heading">
-							<span className="text-muted">See for yourself.</span>
-						</h2>
-						<p className="lead">
-							Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
-							semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus,
-							tellus ac cursus commodo.
-						</p>
-					</div>
-					<div className="col-md-5 order-md-1">
-						<img
-							className="featurette-image img-fluid mx-auto"
-							data-src="holder.js/500x500/auto"
-							alt="Generic placeholder image"
-						/>
-					</div>
-				</div>
-				<hr className="featurette-divider" />
-				<div className="row featurette">
-					<div className="col-md-7">
-						<h2 className="featurette-heading">
-							And lastly, this one. <span className="text-muted">Checkmate.</span>
-						</h2>
-						<p className="lead">
-							Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
-							semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus,
-							tellus ac cursus commodo.
-						</p>
-					</div>
-					<div className="col-md-5">
-						<img
-							className="featurette-image img-fluid mx-auto"
-							data-src="holder.js/500x500/auto"
-							alt="Generic placeholder image"
-						/>
-					</div>
-				</div>
-				<hr className="featurette-divider" />
-				{/* /END THE FEATURETTES */}
 			</div>
-		</main>
-		{/* Bootstrap core JavaScript
-    ================================================== */}
-		{/* Placed at the end of the document so the pages load faster */}
-		{/* Just to make our placeholder images work. Don't actually copy the next line! */}
-	</div>
-);
+			<div className="jumbotron2 jumbotron-fluid">
+				<div className="container">
+					<h1 className="display-4">Fluid jumbotron</h1>
+					<p className="lead">
+						This is a modified jumbotron that occupies the entire horizontal space of its parent.
+					</p>
+				</div>
+			</div>
+			<div className="jumbotron3 jumbotron-fluid">
+				<div className="container">
+					<h1 className="display-4">Fluid jumbotron</h1>
+					<p className="lead">
+						This is a modified jumbotron that occupies the entire horizontal space of its parent.
+					</p>
+				</div>
+			</div>
+			<div className="jumbotron4 jumbotron-fluid">
+				<div className="container">
+					<h1 className="display-4">Fluid jumbotron</h1>
+					<p className="lead">
+						This is a modified jumbotron that occupies the entire horizontal space of its parent.
+					</p>
+				</div>
+			</div>
+			<div className="jumbotron5 jumbotron-fluid">
+				<div className="container">
+					<h1 className="display-4">Fluid jumbotron</h1>
+					<p className="lead">
+						This is a modified jumbotron that occupies the entire horizontal space of its parent.
+					</p>
+				</div>
+			</div>
+			<div className="jumbotron6 jumbotron-fluid">
+				<div className="container">
+					<h1 className="display-4">Fluid jumbotron</h1>
+					<p className="lead">
+						This is a modified jumbotron that occupies the entire horizontal space of its parent.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
