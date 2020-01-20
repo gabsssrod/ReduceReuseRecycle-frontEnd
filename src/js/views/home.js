7cf69440c8d26d8d6c2b0749bbaf2b3f97b87830
JordanAguiriano <JordanAguiriano@hotmail.com>
2020-01-20 19:19:45 +0000

    gabywaby

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,17 +3,17 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import "../../styles/navbar.scss";
 //import { Link } from "react-router";
-import { HashLink as Link } from "react-router-hash-link";
+//import { HashLink as Link } from "react-router-hash-link";
 
 export const Home = () => {
 	return (
 		<div>
 			<div className="jumbotron1 jumbotron-fluid">
 				<div className="container">
-					<h1 className="display-4">Ygab 3000</h1>
-					<p className="lead">the ygab 3000 y not 420??</p>
+					<h1 className="display-4">Ybag 3000</h1>
+					<p className="lead">the ybag3000 y not 420??</p>
 					<button type="button" className="btn btn-light">
-						<Link to="#earthInfo"> Learn More</Link>
+						Learn More
 					</button>
 				</div>
 			</div>
