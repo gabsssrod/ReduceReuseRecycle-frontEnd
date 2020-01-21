a28c68926cb8a721d6c9384bea3187c70de70c21
gabsrodriguez <gmederos95@gmail.com>
2020-01-21 17:09:32 +0000

    search bar

--- src/js/views/SearchItem.js
+++ src/js/views/SearchItem.js
@@ -6,17 +6,10 @@ import "../../styles/searchItems.scss";
 export const SearchItem = () => {
 	return (
 		<>
-			<form className="form-inline my-2 my-lg-0">
-				<input
-					className="search-input form-control mr-sm-2"
-					type="search"
-					placeholder="Search"
-					aria-label="Search"
-				/>
-				<button className="btn1 btn-light my-2 my-sm-0" type="submit">
-					Search
-				</button>
-			</form>
+			<div className="input-field">
+				<input className="search-input" type="text" placeholder="search item" />
+				<div className="icon-wrap" />
+			</div>
 		</>
 	);
 };
