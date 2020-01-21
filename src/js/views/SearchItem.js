f0a9b1e6ef79d906f573da021a5e48cece99bb9f
gabsrodriguez <gmederos95@gmail.com>
2020-01-21 17:29:48 +0000

    search

--- src/js/views/SearchItem.js
+++ src/js/views/SearchItem.js
@@ -5,14 +5,15 @@ import "../../styles/searchItems.scss";
 
 export const SearchItem = () => {
 	return (
-		<form className="form-inline">
-			<i className="fas fa-search" aria-hidden="true" />
-			<input
-				className="form-control search-input form-control-sm ml-3 w-75"
-				type="text"
-				placeholder="Search"
-				aria-label="Search"
-			/>
-		</form>
+		<div className="search">
+			<form className="search1 form-inline">
+				<i className="fas fa-search" aria-hidden="true" />
+				<input
+					className="form-control search-input form-control-sm ml-3 w-75"
+					type="text"
+					aria-label="Search"
+				/>
+			</form>
+		</div>
 	);
 };
