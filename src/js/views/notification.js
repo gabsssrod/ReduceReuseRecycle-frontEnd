72fcf3e166216aad28d4a4747dc655599f046f36
gabsrodriguez <gmederos95@gmail.com>
2020-01-21 21:47:32 +0000

    no jumbo

--- src/js/views/notification.js
+++ src/js/views/notification.js
@@ -15,36 +15,34 @@ export const Notification = () => {
 	let days = ["Monday", "Tuesday", "wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 	return (
-		<div className="jumbotron jumbotron-fluid">
-			<div className="container text-center">
-				<h1 className="display-4 text-center">Your pick up days</h1>
-				<p className="lead" />
-				<button className={button} onClick={collapseHandler}>
-					setup up pick up days
-				</button>
-				<div className={collapse}>
-					<div className="row">
-						<select className="col-6 select1 form-control form-control-sm">
-							<option>Select your first pick up day</option>
-							{days.map((item, index) => {
-								return (
-									<option key={index} value={item}>
-										{item}
-									</option>
-								);
-							})}
-						</select>
-						<select className="cold-6 select2 form-control form-control-sm">
-							<option>Select your second pick up day</option>
-							{days.map((item, index) => {
-								return (
-									<option key={index} value={item}>
-										{item}
-									</option>
-								);
-							})}
-						</select>
-					</div>
+		<div className="container text-center">
+			<h1 className="display-4 text-center">Your pick up days</h1>
+			<p className="lead" />
+			<button className={button} onClick={collapseHandler}>
+				setup up pick up days
+			</button>
+			<div className={collapse}>
+				<div className="row">
+					<select className="col-6 select1 form-control form-control-sm">
+						<option>Select your first pick up day</option>
+						{days.map((item, index) => {
+							return (
+								<option key={index} value={item}>
+									{item}
+								</option>
+							);
+						})}
+					</select>
+					<select className="cold-6 select2 form-control form-control-sm">
+						<option>Select your second pick up day</option>
+						{days.map((item, index) => {
+							return (
+								<option key={index} value={item}>
+									{item}
+								</option>
+							);
+						})}
+					</select>
 				</div>
 			</div>
 		</div>
