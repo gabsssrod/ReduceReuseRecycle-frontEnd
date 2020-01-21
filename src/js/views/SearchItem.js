8d9983ba78776e431185edc9d288626d683121a9
gabsrodriguez <gmederos95@gmail.com>
2020-01-21 17:16:28 +0000

    search bar

--- src/js/views/SearchItem.js
+++ src/js/views/SearchItem.js
@@ -5,11 +5,14 @@ import "../../styles/searchItems.scss";
 
 export const SearchItem = () => {
 	return (
-		<>
-			<div className="input-field">
-				<input className="search-input" type="text" placeholder="search item" />
-				<div className="icon-wrap" />
-			</div>
-		</>
+		<form className="form-inline">
+			<i className="fas fa-search" aria-hidden="true" />
+			<input
+				className="form-control search-input form-control-sm ml-3 w-75"
+				type="text"
+				placeholder="Search"
+				aria-label="Search"
+			/>
+		</form>
 	);
 };
