f4ccc5c5bd3013fd49820340522bf5c33554cf8e
ColbyWTaylor <colbyt.093@gmail.com>
2020-01-29 14:28:49 +0000

    ui enhance

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,5 +4,47 @@ import { Link } from "react-router-dom";
 import "../../styles/navbar.scss";
 
 export const Navbar = () => {
-	return <header />;
+	return (
+		<>
+			<ul className="nav">
+				<li className="nav-item">
+					<Link to="/" className="nav-link">
+						Home
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/notification" className="nav-link">
+						Notifications
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/profil" className="nav-link">
+						My Profile
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/progress" className="nav-link">
+						Progress
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/events" className="nav-link">
+						Events
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/registration" className="nav-link">
+						Register
+					</Link>
+				</li>
+			</ul>
+			{/* 
+			<Link to="/notification">Notifications</Link>
+			<Link to="/login">Login</Link>
+			<Link to="/profile">My Profile</Link>
+			<Link to="/progress">Progress</Link>
+			<Link to="/events">Events</Link>
+			<Link to="/registration">Register</Link> */}
+		</>
+	);
 };
