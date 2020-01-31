12dae6f68c5fc570b622199ef7b811ed5f255314
ColbyWTaylor <colbyt.093@gmail.com>
2020-01-31 15:57:22 +0000

    single item component from flux

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -7,15 +7,39 @@ export const Single = props => {
 	const { store, actions } = useContext(Context);
 	return (
 		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[props.match.params.theid].title}</h1>
+			<h1 className="display-4">{store.materials[props.match.params.theid].name}</h1>
+
+			<h3>
+				<i className="fas fa-recycle mr-1" float="left" />
+				Reduce
+			</h3>
+			<p>{store.materials[props.match.params.theid].reduce}</p>
+
+			<h3>
+				<i className="fas fa-recycle mr-1" float="left" />
+				Reuse
+			</h3>
+			<p>{store.materials[props.match.params.theid].reuse}</p>
+
+			<h3>
+				<i className="fas fa-recycle mr-1" float="left" />
+				Recycle
+			</h3>
+			<p>{store.materials[props.match.params.theid].recycle}</p>
+
+			<h3>
+				<i className="far fa-frown mr-1" float="left" />
+				Trash
+			</h3>
+			<p>{store.materials[props.match.params.theid].trash}</p>
 
 			<hr className="my-4" />
-
+			{/* 
 			<Link to="/">
 				<span className="btn btn-primary btn-lg" href="#" role="button">
 					Back home
 				</span>
-			</Link>
+			</Link> */}
 		</div>
 	);
 };
