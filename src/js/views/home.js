299a99333d3c51f14d3b03b887a1bd87eec12b12
JordanAguiriano <JordanAguiriano@hotmail.com>
2020-02-11 16:45:26 +0000

    landing page edits

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -33,33 +33,60 @@ export const Home = () => {
 				</div>
 				<div className="jumbotron2 jumbotron-fluid" id="earthInfo">
 					<div className="container">
-						<h1 className="display-4">About Us </h1>
-						<p className="lead">
-							This is a modified jumbotron that occupies the entire horizontal space of its parent.
-						</p>
+						<>
+							<h1 className="display-4">Be Mindful</h1>
+							<p className="lead">
+								Being mindful means to be conscious or aware of something. At ReMindful we are aware
+								that many people do not know the proper way to recycle. This does not mean that people
+								are not interested in recycling. We believe that people would be more likely to recycle
+								if they have the resources available. Our mission is to provide knowledge about
+								recycling in a frictionless manner so that we can provide a smooth and easy recyling
+								experience, and leave a better planet for our children.
+							</p>
+						</>
 					</div>
 				</div>
-				<div className="jumbotron3 jumbotron-fluid">
-					<div className="container">
-						<h1 className="display-4">Reduce</h1>
-						<p className="lead">
-							The most effective way to reduce waste is to not create it in the first place. Making a new
-							product requires a lot of materials and energy - raw materials must be extracted from the
-							earth, and the product must be fabricated then transported to wherever it will be sold. As a
-							result, reduction and reuse are the most effective ways you can save natural resources,
-							protect the environment and save money.
-							<Link to="https://www.epa.gov/recycle/reducing-and-reusing-basics">
-								<button>Learn More</button>
-							</Link>
-						</p>
+				<div className="row">
+					<div className="jumbotron3 jumbotron-fluid col-6">
+						<div className="container">
+							<h1 className="display-4" />
+							<p className="lead" />
+						</div>
+					</div>
+					<div className="jumbotron33 jumbotron-fluid col-6">
+						<div className="container">
+							<h1 className="display-4">Reduce</h1>
+							<p className="lead">
+								The most effective way to reduce waste is to not create it in the first place. Making a
+								new product requires a lot of materials and energy - raw materials must be extracted
+								from the earth, and the product must be fabricated then transported to wherever it will
+								be sold. As a result, reduction and reuse are the most effective ways you can save
+								natural resources, protect the environment and save money.
+								<Link to="https://www.epa.gov/recycle/reducing-and-reusing-basics">
+									<button>Learn More</button>
+								</Link>
+							</p>
+						</div>
 					</div>
 				</div>
-				<div className="jumbotron4 jumbotron-fluid">
-					<div className="container">
-						<h1 className="display-4">Reuse</h1>
-						<p className="lead">
-							This is a modified jumbotron that occupies the entire horizontal space of its parent.
-						</p>
+				<div className="row">
+					<div className="jumbotron4 jumbotron-fluid col-6">
+						<div className="container">
+							<h1 className="display-4">Reuse</h1>
+							<p className="lead">
+								Reuse is a means to prevent solid waste from entering the landfill, improve our
+								communities, and increase the material, educational and occupational wellbeing of our
+								citizens by taking useful products discarded by those who no longer want them and
+								providing them to those who do. It is best to reduce first, reuse as a second option,
+								then to resort to recycling.
+							</p>
+						</div>
+					</div>
+					<div className="jumbotron44 jumbotron-fluid col-6">
+						<div className="container">
+							<h1 className="display-4" />
+							<p className="lead" />
+						</div>
 					</div>
 				</div>
 				<div className="jumbotron5 jumbotron-fluid">
@@ -67,8 +94,11 @@ export const Home = () => {
 						<h1 className="display-4">Recycle</h1>
 						<p className="lead">
 							Recycling is the process of collecting and processing materials that would otherwise be
-							thrown away as trash and turning them into new products. Recycling can benefit your
-							community and the environment.
+							thrown away as trash and turning them into new products. Recycling is very important as
+							waste has a huge negative impact on the natural environment. Harmful chemicals and
+							greenhouse gasses are released from rubbish in landfill sites. Recycling helps to reduce the
+							pollution caused by waste. Habitat destruction and global warming are some the affects
+							caused by deforestation.
 						</p>
 					</div>
 				</div>
